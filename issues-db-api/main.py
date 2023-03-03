ba7cfb14fad6d0ee0319d87e5cbd65864244741b
GWNLekkah <a.j.dekker.5@student.rug.nl>
2023-03-03 20:55:54 +0100

    Converted db api to docker container and changed response formats

--- issues-db-api/main.py
+++ issues-db-api/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from pymongo import MongoClient
+import os
 
 app = FastAPI()
 
 # Database setup
-client = MongoClient('mongodb://localhost:27017')
+# client = MongoClient('mongodb://localhost:27017')
+client = MongoClient(os.environ['MONGO_URL'])
 database = client['IssueLabels']
 collection = database['ManualLabels']
 jira_repos = client['JiraRepos']
@@ -13,17 +15,24 @@ jira_repos = client['JiraRepos']
 @app.get('/issue-keys')
 async def issue_keys(request: Request):
     '''
-    TODO: add support for PROJECT
+    Returns the issue keys for which the issue tags match
+    the provided filtering options. These filtering options are
+    given in the body of the request.
     TODO: Fix input validation
     '''
     filter = await request.json()
-    issues = collection.find(filter)
-    return list(issues)
+    issues = collection.find(
+        filter,
+        {'_id'}
+    )
+    return [issue['_id'] for issue in issues]
 
 
 @app.get('/manual-labels')
 async def manual_labels(request: Request):
     '''
+    Returns the manual labels of the issue keys that were
+    provided in the request body.
     TODO: Fix input validation
     '''
     keys = await request.json()
@@ -31,27 +40,37 @@ async def manual_labels(request: Request):
         {'_id': {'$in': keys}},
         ['existence', 'property', 'executive']
     )
-    return list(issues)
+
+    # Build and send response
+    response = {}
+    for issue in issues:
+        response[issue['_id']] = {
+            'existence': issue['existence'],
+            'property': issue['property'],
+            'executive': issue['executive']
+        }
+    return response
 
 
 @app.get('/issue-data')
 async def issue_data(request: Request):
     data = await request.json()
     keys = data['keys']
-    attributes = [f'fields.{attr}' for attr in data['attributes']]
-    print(keys)
-    print(attributes)
 
     issues = []
     for repo in jira_repos.list_collection_names():
         repo_issues = jira_repos[repo].find(
             {'key': {'$in': keys}},
-            attributes
+            ['key'] + [f'fields.{attr}' for attr in data['attributes']]
         )
         issues.extend(list(repo_issues))
-    for idx in range(len(issues)):
-        del issues[idx]['_id']
-    return issues
+    response = {}
+    for issue in issues:
+        attributes = {}
+        for attr in data['attributes']:
+            attributes[attr] = issue['fields'][attr]
+        response[issue['key']] =  attributes
+    return response
 
 
 @app.get('/save-predictions')
@@ -82,3 +101,17 @@ async def add_tags(request: Request):
             {'$set': {'tags': tags}}
         )
     # TODO: what to do with error handling here?
+
+
+@app.get('/fix-project-tags')
+def add_project_tags():
+    issues = collection.find({})
+    for issue in issues:
+        project = issue['_id'].split('-')[0]
+        tags = issue['tags']
+        if project not in tags:
+            tags.append(project)
+        collection.update_one(
+            {'_id': issue['_id']},
+            {'$set': {'tags': tags}}
+        )
