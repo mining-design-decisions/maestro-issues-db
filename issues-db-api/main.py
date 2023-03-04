c9196501dfd63fde3dcfefdac6f902e5d4c2e8c8
GWNLekkah <a.j.dekker.5@student.rug.nl>
2023-03-04 14:38:40 +0100

    Completed the api documentation

--- issues-db-api/main.py
+++ issues-db-api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from pydantic import BaseModel
 from pymongo import MongoClient
 import os
 
@@ -12,76 +13,190 @@ collection = database['ManualLabels']
 jira_repos = client['JiraRepos']
 
 
-@app.get('/issue-keys')
-async def issue_keys(request: Request):
+class IssueKeysIn(BaseModel):
+    filter: dict = {
+        "$and": [
+            {"tags": {"$eq": "tag1"}},
+            {"$or": [
+                {"tags": {"$ne": "tag2"}},
+                {"tags": {"$eq": "tag3"}}
+            ]}
+        ]
+    }
+
+
+class IssueKeysOut(BaseModel):
+    keys: list[str] = [
+        'ISSUE-1',
+        'ISSUE-2'
+    ]
+
+
+@app.get('/issue-keys', response_model=IssueKeysOut)
+def issue_keys(request: IssueKeysIn):
     '''
     Returns the issue keys for which the issue tags match
     the provided filtering options. These filtering options are
     given in the body of the request.
     TODO: Fix input validation
     '''
-    filter = await request.json()
+    filter = request.filter
     issues = collection.find(
         filter,
         {'_id'}
     )
-    return [issue['_id'] for issue in issues]
+    response = IssueKeysOut()
+    response.keys = [issue['_id'] for issue in issues]
+    return response
 
 
-@app.get('/manual-labels')
-async def manual_labels(request: Request):
+class ManualLabelsIn(BaseModel):
+    keys: list[str] = [
+        'ISSUE-1',
+        'ISSUE-2'
+    ]
+
+
+class ManualLabelsOut(BaseModel):
+    labels: dict = {
+        'ISSUE-1': {
+            'existence': True,
+            'property': False,
+            'executive': False
+        },
+        'ISSUE-2': {
+            'existence': False,
+            'property': True,
+            'executive': False
+        }
+    }
+
+
+@app.get('/manual-labels', response_model=ManualLabelsOut)
+def manual_labels(request: ManualLabelsIn):
     '''
     Returns the manual labels of the issue keys that were
     provided in the request body.
     TODO: Fix input validation
     '''
-    keys = await request.json()
     issues = collection.find(
-        {'_id': {'$in': keys}},
+        {'_id': {'$in': request.keys}},
         ['existence', 'property', 'executive']
     )
 
     # Build and send response
-    response = {}
+    response = ManualLabelsOut()
+    labels = {}
     for issue in issues:
-        response[issue['_id']] = {
+        labels[issue['_id']] = {
             'existence': issue['existence'],
             'property': issue['property'],
             'executive': issue['executive']
         }
+    response.labels = labels
     return response
 
 
-@app.get('/issue-data')
-async def issue_data(request: Request):
-    data = await request.json()
-    keys = data['keys']
+class IssueDataIn(BaseModel):
+    keys: list[str] = [
+        'ISSUE-1',
+        'ISSUE-2'
+    ]
+    attributes: list[str] = [
+        'summary',
+        'description'
+    ]
 
+class IssueDataOut(BaseModel):
+    data: dict = {
+        'ISSUE-1': {
+            'summary': 'Summary of ISSUE-1',
+            'description': 'Description of ISSUE-1'
+        },
+        'ISSUE-2': {
+            'summary': 'Summary of ISSUE-2',
+            'description': 'Description of ISSUE-2'
+        }
+    }
+
+
+@app.get('/issue-data', response_model=IssueDataOut)
+def issue_data(request: IssueDataIn):
+    '''
+    Returns issue data. The returned data is determined by the
+    specified issue keys and the attributes that are requested.
+    '''
     issues = []
     for repo in jira_repos.list_collection_names():
         repo_issues = jira_repos[repo].find(
-            {'key': {'$in': keys}},
-            ['key'] + [f'fields.{attr}' for attr in data['attributes']]
+            {'key': {'$in': request.keys}},
+            ['key'] + [f'fields.{attr}' for attr in request.attributes]
         )
         issues.extend(list(repo_issues))
-    response = {}
+    
+    # Build and send response
+    response = IssueDataOut()
+    data = {}
     for issue in issues:
         attributes = {}
-        for attr in data['attributes']:
+        for attr in request.attributes:
             attributes[attr] = issue['fields'][attr]
-        response[issue['key']] =  attributes
+        data[issue['key']] =  attributes
+    response.data = data
     return response
 
 
-@app.get('/save-predictions')
-async def save_predictions(request: Request):
-    return []
+class SavePredictionsIn(BaseModel):
+    model: str = 'MODEL-1'
+    predictions: dict = {
+        'ISSUE-1': {
+            'existence': True,
+            'property': False,
+            'executive': False
+        },
+        'ISSUE-2': {
+            'existence': False,
+            'property': True,
+            'executive': False
+        }
+    }
+
+
+@app.put('/save-predictions')
+def save_predictions(request: SavePredictionsIn):
+    '''
+    Saves the given predictions for the given model.
+    '''
+    model_collection = client['PredictedLabels'][request.model]
+    for key in request.predictions:
+        model_collection.insert_one({
+            '_id': key,
+            'existence': request.predictions[key]['existence'],
+            'property': request.predictions[key]['property'],
+            'executive': request.predictions[key]['executive']
+        })
+
+
+class AddTags(BaseModel):
+    data = {
+        'ISSUE-1': [
+            'TAG-1',
+            'TAG-2'
+        ],
+        'ISSUE-2': [
+            'TAG-3',
+            'TAG-4'
+        ]
+    }
 
 
 @app.patch('/add-tags')
-async def add_tags(request: Request):
-    data = await request.json()
-    for key in data:
+def add_tags(request: AddTags):
+    '''
+    Method for adding tags to issues in bulk. The tags and
+    issue keys should be specified in the request body.
+    '''
+    for key in request.data:
         # Get the current list of tags of this issue
         issue = collection.find_one(
             {'_id': key},
@@ -90,7 +205,7 @@ async def add_tags(request: Request):
         tags = issue['tags']
 
         # Add the new tags
-        for tag in data[key]:
+        for tag in request.data[key]:
             # Only add non-existing tags
             if tag not in tags:
                 tags.append(tag)
@@ -103,8 +218,12 @@ async def add_tags(request: Request):
     # TODO: what to do with error handling here?
 
 
-@app.get('/fix-project-tags')
-def add_project_tags():
+@app.patch('/fix-project-tags')
+def fix_project_tags():
+    '''
+    This method adds the project to the tags field
+    for each issue in the manual labels database.
+    '''
     issues = collection.find({})
     for issue in issues:
         project = issue['_id'].split('-')[0]
