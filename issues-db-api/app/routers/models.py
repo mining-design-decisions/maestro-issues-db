c5fb14c87950bbd183b9008c479703655a04117c
GWNLekkah <a.j.dekker.5@student.rug.nl>
2023-03-09 16:02:44 +0100

    Changed files endpoint to versions endpoint

--- issues-db-api/app/routers/models.py
+++ issues-db-api/app/routers/models.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 from app.dependencies import mongo_client, fs
 from bson import ObjectId
 from dateutil import parser
-import datetime
 
 router = APIRouter()
 example_request = {
@@ -60,59 +59,59 @@ class PutModelOut(BaseModel):
     id: str
 
 
-@router.put('/models')
+@router.post('/models')
 def put_model(request: PutModelIn) -> PutModelOut:
     """
     Creates a new model entry with the given config.
     """
     _id = mongo_client['Models']['ModelInfo'].insert_one({
         'config': request.config,
-        'files': []
+        'versions': []
     }).inserted_id
     return PutModelOut(id=str(_id))
 
 
-@router.put('/models/{model_id}/files')
-def put_model_file(model_id: str, time: str = Form(), file: UploadFile = Form()):
+@router.post('/models/{model_id}/versions')
+def put_model_version(model_id: str, time: str = Form(), file: UploadFile = Form()):
     """
-    Upload a new savefile for the given model-id.
+    Upload a new version for the given model-id.
     """
-    file_id = fs.put(file.file, filename=file.filename)
+    version_id = fs.put(file.file, filename=file.filename)
     mongo_client['Models']['ModelInfo'].update_one(
         {'_id': ObjectId(model_id)},
-        {'$push': {'files': {
-            'id': file_id,
+        {'$push': {'versions': {
+            'id': version_id,
             'time': parser.parse(time)
         }}}
     )
     return {
-        'file-id': str(file_id)
+        'version-id': str(version_id)
     }
 
 
-@router.get('/models/{model_id}/files/{file_id}')
-def get_model_file(model_id: str, file_id: str):
+@router.get('/models/{model_id}/versions/{version_id}')
+def get_model_version(model_id: str, version_id: str):
     """
-    Get the requested savefile for the given model.
+    Get the requested version for the given model.
     """
     model = mongo_client['Models']['ModelInfo'].find_one(
         {'_id': ObjectId(model_id)},
-        ['files']
+        ['versions']
     )
     if model is None:
         raise Exception(f'Model {model_id} not found')
-    for file in model['files']:
-        if file_id == str(file['id']):
-            mongo_file = fs.get(file['id'])
+    for version in model['versions']:
+        if version_id == str(version['id']):
+            mongo_file = fs.get(version['id'])
             return Response(mongo_file.read(),
                             media_type='application/octet-stream')
-    raise Exception(f'File {file_id} not found for model {model_id}')
+    raise Exception(f'Version {version_id} not found for model {model_id}')
 
 
 class GetModelOut(BaseModel):
     id: str
     config: dict
-    files: typing.Any
+    versions: typing.Any
 
 
 @router.get('/models/{model_id}')
@@ -120,16 +119,16 @@ def get_model(model_id: str) -> GetModelOut:
     model = mongo_client['Models']['ModelInfo'].find_one({
         '_id': ObjectId(model_id)
     })
-    files = []
-    for file in model['files']:
-        files.append({
-            'id': str(file['id']),
-            'time': file['time'].isoformat()
+    versions = []
+    for version in model['versions']:
+        versions.append({
+            'id': str(version['id']),
+            'time': version['time'].isoformat()
         })
     return GetModelOut(
         id=model_id,
         config=model['config'],
-        files=files
+        versions=versions
     )
 
 
@@ -151,22 +150,22 @@ class PostPredictionsIn(BaseModel):
     predictions: dict[str, dict[str, dict[str, typing.Any]]]
 
 
-@router.post('/models/{model_id}/predictions')
-def post_predictions(model_id: str, request: PostPredictionsIn):
+@router.post('/models/{model_id}/versions/{version_id}/predictions')
+def post_predictions(model_id: str, version_id: str, request: PostPredictionsIn):
     for issue_id, predicted_classes in request.predictions.items():
         issue = {'_id': issue_id}
         for predicted_class in predicted_classes:
             issue[predicted_class] = predicted_classes[predicted_class]
-        mongo_client['PredictedLabels'][model_id].insert_one(issue)
+        mongo_client['PredictedLabels'][f'{model_id}-{version_id}'].insert_one(issue)
 
 
 class GetPredictionsOut(BaseModel):
     predictions: dict[str, dict[str, dict[str, typing.Any]]]
 
 
-@router.get('/models/{model_id}/predictions')
-def get_predictions(model_id: str):
-    issues = mongo_client['PredictedLabels'][model_id].find({})
+@router.get('/models/{model_id}/versions/{version_id}/predictions')
+def get_predictions(model_id: str, version_id: str):
+    issues = mongo_client['PredictedLabels'][f'{model_id}-{version_id}'].find({})
     predictions = dict()
     for issue in issues:
         issue_id = issue.pop('_id')
