e69a560ddac802cf9820dcb6c117971e2951b561
Arjan <a.j.dekker.5@student.rug.nl>
2023-04-23 16:50:39 +0200

    Added better author tag handling

--- issues-db-api/app/routers/tags.py
+++ issues-db-api/app/routers/tags.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
-from app.dependencies import tags_collection, projects_collection, issue_labels_collection
+from app.dependencies import (
+    tags_collection,
+    projects_collection,
+    issue_labels_collection,
+    users_collection,
+)
 from app.routers.authentication import validate_token
 from pymongo.errors import DuplicateKeyError
 from app.exceptions import tag_exists_exception, tag_not_found_exception
 
-router = APIRouter(
-    prefix='/tags',
-    tags=['tags']
-)
+router = APIRouter(prefix="/tags", tags=["tags"])
 
 
 class NewTag(BaseModel):
@@ -38,7 +40,7 @@ class TagOut(BaseModel):
     tag: DbTag
 
 
-@router.get('', response_model=TagsOut)
+@router.get("", response_model=TagsOut)
 def get_tags():
     """
     Retrieve all unique tags in the database.
@@ -46,69 +48,71 @@ def get_tags():
     tags = tags_collection.find({})
     response = []
     for tag in tags:
-        response.append(DbTag(
-            name=tag['_id'],
-            description=tag['description'],
-            type=tag['type']
-        ))
+        response.append(
+            DbTag(name=tag["_id"], description=tag["description"], type=tag["type"])
+        )
     projects = projects_collection.find({})
     for project in projects:
-        response.append(DbTag(
-            name=project['_id'],
-            description='',
-            type='project'
-        ))
+        response.append(DbTag(name=project["_id"], description="", type="project"))
+    users = users_collection.find({})
+    for user in users:
+        response.append(DbTag(name=user["_id"], description="", type="author"))
     return TagsOut(tags=response)
 
 
-@router.post('')
+@router.post("")
 def create_tag(tag: NewTag, token=Depends(validate_token)):
     """
     Create a new manual tag with the given description.
     """
-    if projects_collection.find_one({'_id': tag.tag}) is not None:
+    if projects_collection.find_one({"_id": tag.tag}) is not None:
+        raise tag_exists_exception(tag.tag)
+    if users_collection.find_one({"_id": tag.tag}) is not None:
         raise tag_exists_exception(tag.tag)
     try:
-        tags_collection.insert_one({
-            '_id': tag.tag,
-            'description': tag.description,
-            'type': 'manual-tag'
-        })
+        tags_collection.insert_one(
+            {"_id": tag.tag, "description": tag.description, "type": "manual-tag"}
+        )
     except DuplicateKeyError:
         raise tag_exists_exception(tag.tag)
 
 
-@router.get('/{tag}', response_model=TagOut)
+@router.get("/{tag}", response_model=TagOut)
 def get_tag(tag: str):
     """
     Retrieve info for the given tag.
     """
-    tag_ = tags_collection.find_one({'_id': tag})
-    if tag_ is None:
-        raise tag_not_found_exception(tag)
-    return TagOut(tag=DbTag(
-        name=tag_['_id'],
-        description=tag_['description'],
-        type=tag_['type']
-    ))
+    tag_ = tags_collection.find_one({"_id": tag})
+    if tag_ is not None:
+        return TagOut(
+            tag=DbTag(
+                name=tag_["_id"], description=tag_["description"], type=tag_["type"]
+            )
+        )
+    tag_ = projects_collection.find_one({"_id": tag})
+    if tag_ is not None:
+        return TagOut(tag=DbTag(name=tag_["_id"], description="", type="project"))
+    tag_ = users_collection.find_one({"_id": tag})
+    if tag_ is not None:
+        return TagOut(tag=DbTag(name=tag_["_id"], description="", type="author"))
+    raise tag_not_found_exception(tag)
 
 
-@router.post('/{tag}')
+@router.post("/{tag}")
 def update_tag(tag: str, request: UpdateTag, token=Depends(validate_token)):
     """
     Retrieve info for the given tag.
     """
     result = tags_collection.update_one(
-        {'_id': tag},
-        {'$set': {'description': request.description}}
+        {"_id": tag}, {"$set": {"description": request.description}}
     )
     if result.matched_count != 1:
         raise tag_not_found_exception(tag)
 
 
-@router.delete('/{tag}')
+@router.delete("/{tag}")
 def delete_tag(tag: str, token=Depends(validate_token)):
-    result = tags_collection.delete_one({'_id': tag})
+    result = tags_collection.delete_one({"_id": tag})
     if result.deleted_count != 1:
         raise tag_not_found_exception(tag)
-    issue_labels_collection.update_many({'tags': tag}, {'$pull': {'tags': tag}})
+    issue_labels_collection.update_many({"tags": tag}, {"$pull": {"tags": tag}})
--- issues-db-api/app/routers/test_tags.py
+++ issues-db-api/app/routers/test_tags.py
@@ -1,36 +1,40 @@
 from .test_util import client
-from .test_util import setup_users_db, restore_dbs, get_auth_header, auth_test_post, auth_test_delete
-from app.dependencies import tags_collection, projects_collection, issue_labels_collection
+from .test_util import (
+    setup_users_db,
+    restore_dbs,
+    get_auth_header,
+    auth_test_post,
+    auth_test_delete,
+)
+from app.dependencies import (
+    tags_collection,
+    projects_collection,
+    issue_labels_collection,
+)
 
 
 def setup_db():
-    tags_collection.insert_one({
-        '_id': 'tag',
-        'description': 'text',
-        'type': 'manual-tag'
-    })
-    projects_collection.insert_one({
-        '_id': 'Apache-HADOOP',
-        'repo': 'Apache',
-        'project': 'HADOOP'
-    })
+    tags_collection.insert_one(
+        {"_id": "tag", "description": "text", "type": "manual-tag"}
+    )
+    projects_collection.insert_one(
+        {"_id": "Apache-HADOOP", "repo": "Apache", "project": "HADOOP"}
+    )
 
 
 def test_get_tags():
     restore_dbs()
     setup_db()
+    setup_users_db()
 
     # Get tags
-    assert client.get('/tags').json() == {
-        'tags': [{
-            'name': 'tag',
-            'description': 'text',
-            'type': 'manual-tag'
-        }, {
-            'name': 'Apache-HADOOP',
-            'description': '',
-            'type': 'project'
-        }]
+    assert client.get("/tags").json() == {
+        "tags": [
+            {"name": "tag", "description": "text", "type": "manual-tag"},
+            {"name": "Apache-HADOOP", "description": "", "type": "project"},
+            {"name": "test", "description": "", "type": "author"},
+            {"name": "other-user", "description": "", "type": "author"},
+        ]
     }
 
     restore_dbs()
@@ -40,40 +44,32 @@ def test_create_tag():
     restore_dbs()
     setup_users_db()
 
-    auth_test_post('/tags')
+    auth_test_post("/tags")
     headers = get_auth_header()
 
     # Create new tag
-    payload = {
-        'tag': 'tag',
-        'description': 'text'
-    }
-    assert client.post('/tags', headers=headers, json=payload).status_code == 200
-    assert tags_collection.find_one({'_id': 'tag'}) == {
-        '_id': 'tag',
-        'description': 'text',
-        'type': 'manual-tag'
+    payload = {"tag": "tag", "description": "text"}
+    assert client.post("/tags", headers=headers, json=payload).status_code == 200
+    assert tags_collection.find_one({"_id": "tag"}) == {
+        "_id": "tag",
+        "description": "text",
+        "type": "manual-tag",
     }
 
     # Duplicate tag
-    assert client.post('/tags', headers=headers, json=payload).status_code == 409
-    assert tags_collection.find_one({'_id': 'tag'}) == {
-        '_id': 'tag',
-        'description': 'text',
-        'type': 'manual-tag'
+    assert client.post("/tags", headers=headers, json=payload).status_code == 409
+    assert tags_collection.find_one({"_id": "tag"}) == {
+        "_id": "tag",
+        "description": "text",
+        "type": "manual-tag",
     }
 
     # Tag in projects collection
-    projects_collection.insert_one({
-        '_id': 'Apache-YARN',
-        'repo': 'Apache',
-        'project': 'YARN'
-    })
-    payload = {
-        'tag': 'Apache-YARN',
-        'description': 'text'
-    }
-    assert client.post('/tags', headers=headers, json=payload).status_code == 409
+    projects_collection.insert_one(
+        {"_id": "Apache-YARN", "repo": "Apache", "project": "YARN"}
+    )
+    payload = {"tag": "Apache-YARN", "description": "text"}
+    assert client.post("/tags", headers=headers, json=payload).status_code == 409
 
     restore_dbs()
 
@@ -83,14 +79,10 @@ def test_get_tag():
     setup_db()
 
     # Get tags
-    assert client.get('/tags/tag').json() == {
-        'tag': {
-            'name': 'tag',
-            'description': 'text',
-            'type': 'manual-tag'
-        }
+    assert client.get("/tags/tag").json() == {
+        "tag": {"name": "tag", "description": "text", "type": "manual-tag"}
     }
-    assert client.get('/tags/non-existent-tag').status_code == 404
+    assert client.get("/tags/non-existent-tag").status_code == 404
 
     restore_dbs()
 
@@ -100,13 +92,23 @@ def test_update_tag():
     setup_users_db()
     setup_db()
 
-    auth_test_post('/tags/add-tags')
+    auth_test_post("/tags/add-tags")
     headers = get_auth_header()
 
     # Get tags
-    assert client.post('/tags/tag', headers=headers, json={'description': 'new-text'}).status_code == 200
-    assert tags_collection.find_one({'_id': 'tag'})['description'] == 'new-text'
-    assert client.post('/tags/non-existent-tag', headers=headers, json={'description': 'new-text'}).status_code == 404
+    assert (
+        client.post(
+            "/tags/tag", headers=headers, json={"description": "new-text"}
+        ).status_code
+        == 200
+    )
+    assert tags_collection.find_one({"_id": "tag"})["description"] == "new-text"
+    assert (
+        client.post(
+            "/tags/non-existent-tag", headers=headers, json={"description": "new-text"}
+        ).status_code
+        == 404
+    )
 
     restore_dbs()
 
@@ -116,21 +118,18 @@ def test_delete_tag():
     setup_users_db()
     setup_db()
 
-    auth_test_delete('/tags/tag')
+    auth_test_delete("/tags/tag")
     headers = get_auth_header()
 
     # Insert issue with the tag
-    issue_labels_collection.insert_one({
-        '_id': 'Apache-01',
-        'tags': ['tag']
-    })
+    issue_labels_collection.insert_one({"_id": "Apache-01", "tags": ["tag"]})
 
     # Delete tag
-    assert client.delete('/tags/tag', headers=headers).status_code == 200
-    assert tags_collection.find_one({'_id': 'tag'}) is None
-    assert issue_labels_collection.find_one({'_id': 'Apache-01'})['tags'] == []
+    assert client.delete("/tags/tag", headers=headers).status_code == 200
+    assert tags_collection.find_one({"_id": "tag"}) is None
+    assert issue_labels_collection.find_one({"_id": "Apache-01"})["tags"] == []
 
     # Non-existing tag
-    assert client.delete('/tags/tag', headers=headers).status_code == 404
+    assert client.delete("/tags/tag", headers=headers).status_code == 404
 
     restore_dbs()
